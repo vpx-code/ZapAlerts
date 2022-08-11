0031b581baece66739b8b6477678de4012dec326
rtfmanual <72383992+rtfmanual@users.noreply.github.com>
2022-08-11 20:37:18 +0200

    DB components organized to fit MVVM principles.

--- app/src/main/java/com/xvlaze/zapalerts/ui/MainViewModel.kt
+++ app/src/main/java/com/xvlaze/zapalerts/ui/MainViewModel.kt
@@ -3,25 +3,13 @@ package com.xvlaze.zapalerts.ui
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import com.xvlaze.zapalerts.model.CloudDB
-import com.xvlaze.zapalerts.model.CloudDBQueries
 import com.xvlaze.zapalerts.model.IOnSuccessListenerCallback
 import com.xvlaze.zapalerts.model.InterestCloudObject
 import com.xvlaze.zapalerts.repository.CloudDBRepository
-import com.xvlaze.zapalerts.repository.Repository
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository = Repository(application.applicationContext)
-    private val cloudDBInstance = CloudDB(application.applicationContext)
-    private var cloudDBQueries: CloudDBQueries
     private val cloudDBRepository = CloudDBRepository(application.applicationContext)
     val savedInterests = MutableLiveData<MutableList<InterestCloudObject>>()
-
-    init {
-        cloudDBInstance.createObjectType()
-        cloudDBInstance.openCloudDbZone()
-        cloudDBQueries = CloudDBQueries(cloudDBInstance.mCloudDbZone!!)
-    }
 
     fun getSavedInterests() {
         cloudDBRepository.getAll(object : IOnSuccessListenerCallback {
