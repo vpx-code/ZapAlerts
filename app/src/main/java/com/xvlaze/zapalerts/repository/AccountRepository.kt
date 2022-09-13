f61ce65e7b36b69accf6805550746134ede66aea
rtfmanual <72383992+rtfmanual@users.noreply.github.com>
2022-09-13 14:04:38 +0200

    Added LoginViewModel and MVVM separation.

---
+++ app/src/main/java/com/xvlaze/zapalerts/repository/AccountRepository.kt
@@ -0,0 +1,9 @@
+package com.xvlaze.zapalerts.repository
+
+import android.content.Intent
+import com.xvlaze.zapalerts.model.AccountModel
+import com.xvlaze.zapalerts.model.IOnUserSignInCallback
+
+class AccountRepository {
+    fun signIn(data: Intent?, callback: IOnUserSignInCallback) = AccountModel.signIn(data, callback)
+}
--- app/src/main/java/com/xvlaze/zapalerts/ui/LoginActivity.kt
+++ app/src/main/java/com/xvlaze/zapalerts/ui/LoginActivity.kt
@@ -3,10 +3,9 @@ package com.xvlaze.zapalerts.ui
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.huawei.agconnect.auth.AGConnectAuth
-import com.huawei.agconnect.auth.HwIdAuthProvider
 import com.huawei.hms.support.hwid.HuaweiIdAuthManager
 import com.huawei.hms.support.hwid.request.HuaweiIdAuthParams
 import com.huawei.hms.support.hwid.request.HuaweiIdAuthParamsHelper
@@ -17,6 +16,7 @@ import com.xvlaze.zapalerts.model.User
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,28 +54,9 @@ class LoginActivity : AppCompatActivity() {
     ) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 100) {
-            val authHuaweiIdTask = HuaweiIdAuthManager.parseAuthResultFromIntent(data)
-            if (authHuaweiIdTask.isSuccessful) {
-                val huaweiAccount = authHuaweiIdTask.result
-                // we need access token to create credential.
-                val accessToken = huaweiAccount.accessToken
-                val credential = HwIdAuthProvider.credentialWithToken(accessToken)
-                AGConnectAuth.getInstance().signIn(credential)
-                    .addOnSuccessListener { signInResult -> // onSuccess
-                        val user = signInResult.user
-                        User.unionId = user.uid
-                        Toast.makeText(this, user.uid, Toast.LENGTH_LONG).show()
-                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                        finish()
-                    }.addOnFailureListener {
-                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
-                    }
-            } else {
-                Toast.makeText(
-                    this,
-                    "HwID signIn failed: " + authHuaweiIdTask.exception.message,
-                    Toast.LENGTH_LONG
-                ).show()
+            viewModel.signIn(data)
+            viewModel.isSignInSuccessful.observe(this) {
+
             }
         }
     }
