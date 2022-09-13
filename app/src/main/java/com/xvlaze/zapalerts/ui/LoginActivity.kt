513023d1369b51e59ace09375d39614a0448afce
rtfmanual <72383992+rtfmanual@users.noreply.github.com>
2022-09-13 13:39:36 +0200

    Transitioned to HUAWEI ID Sign In instead of SMS.

--- app/src/main/java/com/xvlaze/zapalerts/ui/LoginActivity.kt
+++ app/src/main/java/com/xvlaze/zapalerts/ui/LoginActivity.kt
@@ -5,10 +5,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.huawei.agconnect.auth.*
-import com.huawei.agconnect.auth.VerifyCodeSettings.ACTION_REGISTER_LOGIN
-import com.huawei.hmf.tasks.Task
-import com.huawei.hmf.tasks.TaskExecutors
+import com.huawei.agconnect.auth.AGConnectAuth
+import com.huawei.agconnect.auth.HwIdAuthProvider
+import com.huawei.hms.support.hwid.HuaweiIdAuthManager
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParams
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParamsHelper
 import com.xvlaze.zapalerts.R
 import com.xvlaze.zapalerts.databinding.ActivityLoginBinding
 import com.xvlaze.zapalerts.model.User
@@ -17,19 +18,14 @@ import com.xvlaze.zapalerts.model.User
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
-    private lateinit var countryCode: String
-    private lateinit var phoneNumber: String
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         if (AGConnectAuth.getInstance().currentUser != null) {
             User.unionId = AGConnectAuth.getInstance().currentUser.uid
             startActivity(Intent(this@LoginActivity, MainActivity::class.java))
         } else {
             binding = ActivityLoginBinding.inflate(layoutInflater)
             setContentView(binding.root)
-
             when (resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
                 Configuration.UI_MODE_NIGHT_YES -> {
                     binding.bg.setImageResource(R.drawable.city_night)
@@ -41,87 +37,46 @@ class LoginActivity : AppCompatActivity() {
                     binding.bg.setImageResource(R.drawable.city_day)
                 }
             }
-
-
-            binding.sendCode.setOnClickListener {
-                val settings = VerifyCodeSettings.newBuilder()
-                    .action(ACTION_REGISTER_LOGIN)
-                    .sendInterval(30)
-                    .build()
-
-                val task: Task<VerifyCodeResult> =
-                    PhoneAuthProvider.requestVerifyCode(
-                        binding.countryPicker.selectedCountryCode,
-                        binding.phoneNumber.text.toString(),
-                        settings
-                    )
-
-                task.addOnSuccessListener(TaskExecutors.uiThread()) {
-                    countryCode = binding.countryPicker.selectedCountryCode
-                    phoneNumber = binding.phoneNumber.text.toString()
-
-                    binding.signUp.setOnClickListener {
-                        signUp()
-                    }
-
-                    binding.signIn.setOnClickListener {
-                        signIn()
-                    }
-                }
+            binding.signIn.setOnClickListener {
+                val authParams =
+                    HuaweiIdAuthParamsHelper(HuaweiIdAuthParams.DEFAULT_AUTH_REQUEST_PARAM).setAccessToken()
+                        .createParams()
+                val service = HuaweiIdAuthManager.getService(this, authParams)
+                startActivityForResult(service.signInIntent, 100)
             }
         }
     }
 
-    private fun signIn() {
-        val credential = PhoneAuthProvider.credentialWithVerifyCode(
-            countryCode,
-            phoneNumber,
-            null,
-            binding.verificationCode.text.toString()
-        )
-
-        AGConnectAuth.getInstance().signIn(credential).addOnSuccessListener {
-            // The verification code application is successful.
-            PhoneUser.Builder()
-                .setCountryCode(countryCode)
-                .setPhoneNumber(phoneNumber) // The value of phoneNumber must contains the country/region code and mobile number.
-                .setVerifyCode(binding.verificationCode.text.toString())
-                .build()
-
-            User.unionId = AGConnectAuth.getInstance().currentUser.uid
-
-            AGConnectAuth.getInstance().signIn(credential) // Fixes initial list is empty after sign in problem! :)
-                .addOnSuccessListener {
-                    //User.unionId = AGConnectAuth.getInstance().currentUser.uid
-                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                    finish()
-                }
-                .addOnFailureListener {
-                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                    finish()
-                }
-        }
-    }
-
-    private fun signUp() {
-        val phoneUser = PhoneUser.Builder()
-            .setCountryCode(countryCode)
-            .setPhoneNumber(phoneNumber) // The value of phoneNumber must contains the country/region code and mobile number.
-            .setVerifyCode(binding.verificationCode.text.toString())
-            .build()
-
-        AGConnectAuth.getInstance().createUser(phoneUser)
-            .addOnSuccessListener {
-                User.unionId = it.user.uid
-                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                finish()
-            }
-            .addOnFailureListener {
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100) {
+            val authHuaweiIdTask = HuaweiIdAuthManager.parseAuthResultFromIntent(data)
+            if (authHuaweiIdTask.isSuccessful) {
+                val huaweiAccount = authHuaweiIdTask.result
+                // we need access token to create credential.
+                val accessToken = huaweiAccount.accessToken
+                val credential = HwIdAuthProvider.credentialWithToken(accessToken)
+                AGConnectAuth.getInstance().signIn(credential)
+                    .addOnSuccessListener { signInResult -> // onSuccess
+                        val user = signInResult.user
+                        User.unionId = user.uid
+                        Toast.makeText(this, user.uid, Toast.LENGTH_LONG).show()
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                        finish()
+                    }.addOnFailureListener {
+                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                    }
+            } else {
                 Toast.makeText(
-                    this@LoginActivity,
-                    "Something went wrong, please try again",
+                    this,
+                    "HwID signIn failed: " + authHuaweiIdTask.exception.message,
                     Toast.LENGTH_LONG
                 ).show()
             }
+        }
     }
 }
