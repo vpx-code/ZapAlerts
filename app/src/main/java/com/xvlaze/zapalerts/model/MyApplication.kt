1396844cf5d9fe484c7dbc11d52395d2c421714b
rtfmanual <72383992+rtfmanual@users.noreply.github.com>
2022-09-23 19:27:33 +0200

    Instrumented my app with New Relic

--- app/src/main/java/com/xvlaze/zapalerts/model/MyApplication.kt
+++ app/src/main/java/com/xvlaze/zapalerts/model/MyApplication.kt
@@ -4,12 +4,18 @@ import android.app.Application
 import android.content.Context
 import android.util.Log
 import com.huawei.hms.searchkit.SearchKitInstance
+import com.newrelic.agent.android.NewRelic
 import com.xvlaze.zapalerts.util.Constants.clientId
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
+
+        NewRelic.withApplicationToken(
+            "eu01xxba32e7845f4dbee7b85aef6d7a6bc17506e6-NRMA"
+        ).start(appContext);
+
         SearchKitInstance.init(this, clientId)
         OAuthTokenProvider.requestOAuthToken()
 
